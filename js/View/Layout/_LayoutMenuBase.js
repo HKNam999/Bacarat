9b4cf4db1cd123b04aeca5d117a3d40bdbf4b1a4
HKNam999 <123411234n@gmail.com>
2025-12-24 13:16:54 +0700

    Upload js/View/Layout/_LayoutMenuBase.js from ZIP file

---
+++ js/View/Layout/_LayoutMenuBase.js
@@ -0,0 +1,47 @@
+﻿$('.countryCode').on('change', function () {
+    var languageCode = $(this).val();
+    if (languageCode) {
+        callPostAPIPublic('/Account/SelectLanguage', { languageCode: languageCode }, function (response) {
+            i18next.changeLanguage(languageCode);
+            $('.countryCode').val(languageCode);
+            location.reload();
+        });
+    }
+})
+
+function initLocalize() {
+    var cookieLanguageCode = $('.countryCode').val(),
+        languageCode = cookieLanguageCode ? cookieLanguageCode : 'vi';
+
+    // Khởi tạo i18next và cấu hình
+    i18next.init({
+        lng: languageCode,
+        debug: true,
+        resources: {
+            en: {
+                translation: translationEN,
+            },
+            vi: {
+                translation: translationVI,
+            },
+            th: {
+                translation: translationTH,
+            },
+            zh: {
+                translation: translationZH,
+            },
+            ko: {
+                translation: translationKR,
+            }
+        },
+    });
+}
+
+function updateCoin(coin) {
+    $('.credit-text').text(coin);
+}
+
+
+$(document).ready(function () {
+    initLocalize();
+})
