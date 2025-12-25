ab2878d1a58e5d42f54e5f62568f1c3df75b15fc
HKNam999 <123411234n@gmail.com>
2025-12-25 12:57:01 +0700

    Upload ae/get_formula_data.php from ZIP file

---
+++ ae/get_formula_data.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Content-Type');
+
+$json = file_get_contents('get_formula_data.json');
+echo $json;
+?>
